e83ba9b02e620d67b28cb541f5082bc85fb7a1cc
facubrusa <facundobrusa11@gmail.com>
2022-09-10 13:32:46 -0300

    modularize user controller

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,11 +1,28 @@
-const store = require('../../../store/dummy');
-
 const TABLE = 'user';
 
-function list() {
-    return store.list(TABLE);
-}
+module.exports = function(injectedStore) {
+    // if the recived store is null, get a common store
+    const store = injectedStore ? injectedStore : require('../../../store/dummy');
+    function list() {
+        return store.list(TABLE);
+    }
 
-module.exports = {
-    list,
+    function get(id) {
+        return store.get(TABLE, id);
+    }
+
+    function insert(name) {
+        return store.insert(TABLE, name);
+    }
+
+    function remove(id) {
+        return store.remove(TABLE, id);
+    }
+
+    return {
+        list,
+        get,
+        insert,
+        remove,
+    };
 };
--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,13 +1,69 @@
 const express = require("express");
 
 const response = require("../../../network/response");
-const Controller = require("./controller");
+const Controller = require("./index");
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  const list = Controller.list();
-  response.success(req, res, list, 200);
+router.get("/", async function (req, res) {
+  try {
+    const userList = await Controller.list();
+    response.success(req, res, userList, 200);
+  } catch (error) {
+    response.error(req, res, error.message, 500);
+  }
+});
+
+router.get("/:id", async function (req, res) {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      response.error(req, res, 'id is required', 400);
+      return;
+    }
+    const user = await Controller.get(id);
+    if (!user) {
+      response.error(req, res, 'User not found', 404);
+      return;
+    }
+    response.success(req, res, user, 200);
+  } catch (error) {
+    response.error(req, res, error.message, 500);
+  }
+});
+
+router.post("/", async function (req, res) {
+  try {
+    console.log(req);
+    const { name } = req.body;
+    if (!name) {
+      response.error(req, res, 'name is required', 400);
+      return;
+    }
+    const newUser = await Controller.insert(name);
+    response.success(req, res, newUser, 200);
+  } catch (error) {
+    response.error(req, res, error.message, 500);
+  }
+});
+
+router.delete("/:id", async function (req, res) {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      response.error(req, res, 'id is required', 400);
+      return;
+    }
+    const user = await Controller.get(id);
+    if (!user) {
+      response.error(req, res, 'User not found', 404);
+      return;
+    }
+    await Controller.delete(id);
+    response.success(req, res, user, 200);
+  } catch (error) {
+    response.error(req, res, error.message, 500);
+  }
 });
 
 module.exports = router;
